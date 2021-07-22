c380b18790a41ef2492cace89079a768a97f23f3
Maximiliano Pérez <g.maximiliano.perez@gmail.com>
2021-07-21 22:36:30 -0300

    Cambios en html

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ let usd = 96.33
 let eu = 113.63
 let real = 18.57
 
+
+document.querySelector('#usd').innerHTML = 'USD : '+usd;
+document.querySelector('#eu').innerHTML = 'EURO : '+eu;
+document.querySelector('#real').innerHTML = 'REAL : '+real;
+
 function convertir(){
     if(ars.value > 0){
        var result = ars.value/usd
