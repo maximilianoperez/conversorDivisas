69304ccbb0fbb6509e34419f552379d6b1bc1918
Maximiliano Pérez <g.maximiliano.perez@gmail.com>
2021-07-21 22:28:18 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let ars = document.getElementById('ars');
+let usd = 96.33
+let eu = 113.63
+let real = 18.57
+
+function convertir(){
+    if(ars.value > 0){
+       var result = ars.value/usd
+       var resultUsd = document.querySelector('#mostrarUsd').innerHTML = 'USD : ' +result.toFixed(2);
+
+       result = ars.value/eu
+       var resultEu = document.querySelector('#mostrarEu').innerHTML = 'EUROS : ' +result.toFixed(2);
+
+       result = ars.value/real
+       var resultReal = document.querySelector('#mostrarReal').innerHTML = 'REAIS : ' +result.toFixed(2);
+       
+    }
+}
