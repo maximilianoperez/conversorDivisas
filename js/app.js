8b2f1002290b197d95a91879328df47a55f3cb21
Maximiliano Pérez <g.maximiliano.perez@gmail.com>
2021-07-23 00:34:58 -0300

    Cambios en main.css

--- js/app.js
+++ js/app.js
@@ -11,13 +11,13 @@ document.querySelector('#real').innerHTML = 'REAL : '+real;
 function convertir(){
     if(ars.value > 0){
        var result = ars.value/usd
-       var resultUsd = document.querySelector('#mostrarUsd').innerHTML = 'USD : ' +result.toFixed(2);
+       var resultUsd = document.querySelector('#mostrarUsd').innerHTML = result.toFixed(2)+ ' USD';
 
        result = ars.value/eu
-       var resultEu = document.querySelector('#mostrarEu').innerHTML = 'EUROS : ' +result.toFixed(2);
+       var resultEu = document.querySelector('#mostrarEu').innerHTML = result.toFixed(2)+ ' EUROS';
 
        result = ars.value/real
-       var resultReal = document.querySelector('#mostrarReal').innerHTML = 'REAIS : ' +result.toFixed(2);
+       var resultReal = document.querySelector('#mostrarReal').innerHTML = result.toFixed(2)+ ' REALES';
        
     }
 }
